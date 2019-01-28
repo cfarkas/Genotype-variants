a49b74085ab6671479f0c053d7f94b8f7fe074bc
cfarkas <carlosfarkas@gmail.com>
2019-01-28 19:16:01 -0300

    Add files via upload

---
+++ Test/Config_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CORES=${1}
+
+# Obtaining mm10.fa genome, and indexing
+
+wget http://hgdownload.cse.ucsc.edu/goldenpath/mm10/bigZips/mm10.2bit
+chmod 755 twoBitToFa
+./twoBitToFa mm10.2bit mm10.fa
+samtools faidx mm10.fa
+
+
+# Pipeline
+
+bash ./sort_bam.sh ${CORES}
+bash ./variant_collection.sh /datos1/genotype_variants_mouse/mm10.fa ${CORES} 
+bash ./filtering_combined_mouse.sh 
+cd vcf_outputs/
+bash ./genotype_variants_mouse.sh WT.filtered.vcf KO.intersection.vcf /datos1/genotype_variants_mouse/mm10.fa
+
+###
