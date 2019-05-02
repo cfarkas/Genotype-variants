96057fdf385b636488287fae534488a148fc9dfc
cfarkas <carlosfarkas@gmail.com>
2019-05-02 15:50:46 -0400

    Update Config_test.sh

--- Test/Config_test.sh
+++ Test/Config_test.sh
@@ -13,9 +13,9 @@ samtools faidx mm10.fa
 # Pipeline
 
 bash ./sort_bam.sh ${CORES}
-bash ./variant_collection.sh /datos1/genotype_variants_mouse/mm10.fa ${CORES} 
+bash ./variant_collection.sh mm10.fa ${CORES} 
 bash ./filtering_combined_mouse.sh 
 cd vcf_outputs/
-bash ./genotype_variants_mouse.sh WT.filtered.vcf KO.intersection.vcf /datos1/genotype_variants_mouse/mm10.fa
+bash ./genotype_variants_mouse.sh WT.filtered.vcf KO.intersection.vcf mm10.fa
 
 ###
